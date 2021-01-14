aca58a559c20b67f5230f4130cbef853c5c2a8ab
kiore <ricardochicas95@gmail.com>
2021-01-14 17:17:45 -0600

    Add Person data model

---
+++ app/src/main/java/com/example/roomapplication/model/Person.kt
@@ -0,0 +1,15 @@
+package com.example.roomapplication.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Person(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val name: String,
+    val age: Int,
+    val address: String
+)
+
+
